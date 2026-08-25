be7b5c5dee8da193330ebf1e21616539a709c9a3
Navin2112 <navinpoki2112@gmail.com>
2026-08-25 16:14:46 +0530

    added js

---
+++ product.js
@@ -0,0 +1,37 @@
+//Selecting side navbar, menuIcon
+var sidenav = document.getElementById("sidenav")
+var menuicon = document.getElementById("menuicon")
+var closenav = document.getElementById("closenav")
+
+
+menuicon.addEventListener("click", function () {
+    sidenav.style.right = 0
+
+})
+
+closenav.addEventListener("click", function () {
+    sidenav.style.right = "-50%"
+})
+
+//product search functionality
+
+var search = document.getElementById("search")
+var productcontainer = document.getElementById("product-container")
+var productlist = productcontainer.querySelectorAll("div")
+
+search.addEventListener("keyup", function () {
+    var enteredvalue = event.target.value.toUpperCase()
+
+    for (count = 0; count<productlist.length; count=count+1) 
+        {
+        var productname = productlist[count].querySelector("h3").textContent
+
+        if (productname.toUpperCase().indexOf(enteredvalue)<0) {
+            productlist[count].style.display="none"
+        }
+        else {
+            productlist[count].style.display= "block"
+        }
+    }
+})
+
